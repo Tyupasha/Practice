ea9036ba2db1c41ed7b444def96d622004d40c76
Dima Tyupa <Dima Tyupa>
2016-02-16 00:13:18 +0300

    Add  clean-code projects

---
+++ CleanCodeProject/Task1.2/src/Message.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Dima on 15.02.2016.
+ */
+public class Message {
+    private String id;
+    private String author;
+    private String message;
+    private long timestamp;
+
+    public Message(String id, String author, String message, long timestamp) {
+        this.id = id;
+        this.author = author;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    String get_id() {
+        return this.id;
+    }
+    String get_author() {
+        return this.author;
+    }
+    String get_message() {
+        return this.message;
+    }
+    long get_timestamp() {
+        return this.timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + this.id + ", author: " + this.author + ", timestamp: " + this.timestamp + ", message: " + this.message;
+    }
+}
