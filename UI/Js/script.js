48308cea1e9468d2f0c53710c80cc953b3d19c9b
Dima Tyupa <Dima Tyupa>
2016-04-08 17:37:52 +0300

    (Task 5) Add LocalStorage

--- UI/Js/script.js
+++ UI/Js/script.js
@@ -2,30 +2,82 @@
  * Created by Dima on 13.03.2016.
  */
 
-function sendMessage() {
+var user;
+var messages = [];
 
+window.onload = function() {
+   var messageList = localStorage.getItem('messages');
+    if (messageList == undefined) {
+        return;
+    }
+    messageList = JSON.parse(messageList);
+    for(var i in messageList) {
+        if(messageList[i].text == 'Deleted') {
+            addDeletedMessageOnUI(messageList[i]);
+        }
+        else {
+            addMessageOnUI(messageList[i]);
+        }
+    }
+    var userList = localStorage.getItem('user');
+    if(userList == undefined) {
+        return;
+    }
+    userList = JSON.parse(userList);
+    user = userList;
+    setUserOnUI(userList);
+};
+
+function sendMessage() {
+    if(user == undefined) {
+        alert("Firstly input your name!");
+        return;
+    }
+
+    if(getMessageText().length > 100) {
+        alert("Too big message!");
+        return;
+    }
+    if(getMessageText() == 0) {
+        alert("You need to input something!");
+        return;
+    }
+
+    var newMessage = {
+        'id': setId(5),
+        'date': new Date().toLocaleString(),
+        'user': user,
+        'text': getMessageText()
+    };
+    saveToLocalStorage(newMessage);
+    addMessageOnUI(newMessage);
+}
+
+function saveToLocalStorage(message) {
+    if (typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+    messages.push(message);
+    localStorage.setItem('messages', JSON.stringify(messages));
+}
+
+function addMessageOnUI(newMessage) {
     var newMessageDiv = document.createElement('div');
     newMessageDiv.className = 'message';
 
     var messageInfoDiv = document.createElement('div');
     messageInfoDiv.className = 'my-message-info';
-    messageInfoDiv.innerHTML = new Date().toLocaleString() + ' <a href = "#" class ="change-mes" title = "Edit message" onclick="editMessage()"><img src = "http://s1.iconbird.com/ico/2013/3/636/w80h8013939672873.png" width=20em height=20em></a>' +
-    '<a href = "#" class ="delete-mes" title = "Delete message" onclick="deleteMessage()"><img src = "http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/bin-full.png" width=20em height=20em></a>';
+    messageInfoDiv.innerHTML = newMessage.date +
+        ' <a href = "#" class ="change-mes" title = "Edit message" onclick="editMessage()">' +
+        '<img src = "http://s1.iconbird.com/ico/2013/3/636/w80h8013939672873.png" width=20em height=20em></a>' +
+        '<a href = "#" class ="delete-mes" title = "Delete message" onclick="deleteMessage()">' +
+        '<img src = "http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/bin-full.png" width=20em height=20em></a>' + newMessage.user;
 
 
     var messageTextDiv = document.createElement('div');
     messageTextDiv.className = 'my-message-text';
-    messageTextDiv.innerHTML = getMessageText();
-
-
-    if(messageTextDiv.innerHTML.length > 100) {
-        alert("Too big message!");
-        return;
-    }
-    if(messageTextDiv.innerHTML.length == 0) {
-        alert("You need to input something!");
-        return;
-    }
+    messageTextDiv.innerHTML = newMessage.text;
 
     newMessageDiv.appendChild(messageInfoDiv);
     newMessageDiv.appendChild(messageTextDiv);
@@ -33,21 +85,12 @@ function sendMessage() {
     var chatPanel = window.document.getElementsByClassName('chat-screen')[0];
     chatPanel.appendChild(newMessageDiv);
 
-    changeUserActivity()
+    changeUserActivity();
 }
 
-function ClearText(Empty) {
-    Empty.value="";
-}
 
-function getMessageText() {
-    var inputElement = document.getElementsByClassName('text-area')[0];
-    return inputElement.value;
-}
-
-function changeUser() {
+function setUser() {
     var newName = prompt("Input your name", "Here: ");
-    var newPanelHead = document.createElement('div');
 
     if(newName == undefined) {
         alert("Input your name!");
@@ -55,9 +98,10 @@ function changeUser() {
     }
 
     if((newName.length > 20) || (newName.length == "")) {
-        alert("Incorrect name!")
+        alert("Incorrect name!");
         return;
     }
+
     var elements = window.document.getElementsByClassName('user');
     for(var i = 0; i < elements.length; i++) {
         if(elements[i].innerHTML == newName) {
@@ -66,14 +110,43 @@ function changeUser() {
         }
     }
 
+    saveUserToLocalStorage(newName);
+    setUserOnUI(newName);
+}
+
+function saveUserToLocalStorage(newUser) {
+    if (typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+    user = newUser;
+    localStorage.setItem('user',JSON.stringify(user));
+}
+
+function setUserOnUI(name) {
+    var newPanelHead = document.createElement('div');
+
     newPanelHead.className = 'user-panel-head';
-    newPanelHead.innerHTML = '<a href = "#" class = "user-activity" title = "Online"><img src ="http://s1.iconbird.com/ico/2013/3/636/w80h80139396727836.png" width=20em height=15em> </a>' + newName +
-    '<a href = "#" class ="change-user" title = "Change user" onclick="changeUser()"><img src ="http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/user-boss.png" width=20em height=20em></a>';
+    var newSpan = document.createElement('span');
+    newSpan.innerHTML = name;
+    newSpan.className = 'user-name';
+    newPanelHead.innerHTML = '<a href = "#" class = "user-activity" title = "Online"><img src ="http://s1.iconbird.com/ico/2013/3/636/w80h80139396727836.png" width=20em height=15em> </a>' + newSpan.innerHTML +
+    '<a href = "#" class ="change-user" title = "Change user" onclick="setUser()"><img src ="http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/user-boss.png" width=20em height=20em></a>';
 
     var userPanel = window.document.getElementsByClassName('user-panel')[0];
     var userPanelHead = window.document.getElementsByClassName('user-panel-head')[0];
 
     userPanel.replaceChild(newPanelHead,userPanelHead);
+}
+
+function editMessageInLocalStorage(index, newText) {
+    var messageListJS = localStorage.getItem('messages');
+    if (messageListJS == undefined) {
+        return;
+    }
+    var messageList = JSON.parse(messageListJS);
+    messageList[index].text = newText;
+    localStorage.setItem('messages', JSON.stringify(messageList));
 }
 
 function editMessage() {
@@ -95,7 +168,7 @@ function editMessage() {
     var messageInfoDiv = document.createElement('div');
     messageInfoDiv.className = 'my-message-info';
     messageInfoDiv.innerHTML = new Date().toLocaleString() + ' <a href = "#" class ="change-mes" title = "Edit message" onclick="editMessage()"><img src = "http://s1.iconbird.com/ico/2013/3/636/w80h8013939672873.png" width=20em height=20em></a>' +
-        '<a href = "#" class ="delete-mes" title = "Delete message" onclick="deleteMessage()"><img src = "http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/bin-full.png" width=20em height=20em></a>';
+        '<a href = "#" class ="delete-mes" title = "Delete message" onclick="deleteMessage()"><img src = "http://www.iconsearch.ru/uploads/icons/49handdrawing/128x128/bin-full.png" width=20em height=20em></a>' + user;
 
     var messageTextDiv = document.createElement('div');
     messageTextDiv.className = 'my-message-text';
@@ -112,23 +185,34 @@ function editMessage() {
     var chatPanel = window.document.getElementsByClassName('chat-screen')[0];
     chatPanel.replaceChild(newMessageDiv,click());
 
-}
-
-function click() {
-    var t = event.target;
-    var par = t.parentNode;
-    var par2 = par.parentNode;
-    var par3 = par2.parentNode;
-    return par3;
+    var index = findMessageInLocalStorage(currentText);
+    editMessageInLocalStorage(index, newText);
 }
 
 function deleteMessage() {
+    var currentText = click().lastChild.innerHTML;
+    var index = findMessageInLocalStorage(currentText);
+    deleteMessageInLocalStorage(index);
+    deleteMessageOnUI();
+}
+
+function deleteMessageInLocalStorage(index) {
+    var messageListJS = localStorage.getItem('messages');
+    if (messageListJS == undefined) {
+        return;
+    }
+    var messageList = JSON.parse(messageListJS);
+    messageList[index].text = 'Deleted';
+    localStorage.setItem('messages', JSON.stringify(messageList));
+}
+
+function deleteMessageOnUI() {
     var newMessageDiv = document.createElement('div');
     newMessageDiv.className = 'del-message';
 
     var mesInfo = document.createElement('div');
     mesInfo.className = 'del-message-info';
-    mesInfo.innerHTML = new Date().toLocaleString();
+    mesInfo.innerHTML = new Date().toLocaleString() + ' ' + user;
 
     var mesText = document.createElement('div');
     mesText.className = 'del-message-text';
@@ -140,6 +224,26 @@ function deleteMessage() {
     var chatPanel = window.document.getElementsByClassName('chat-screen')[0];
 
     chatPanel.replaceChild(newMessageDiv,click());
+}
+
+function addDeletedMessageOnUI(message) {
+    var newMessageDiv = document.createElement('div');
+    newMessageDiv.className = 'del-message';
+
+    var mesInfo = document.createElement('div');
+    mesInfo.className = 'del-message-info';
+    mesInfo.innerHTML = message.date + ' ' + message.user;
+
+    var mesText = document.createElement('div');
+    mesText.className = 'del-message-text';
+    mesText.innerHTML = 'This message has been deleted';
+
+    newMessageDiv.appendChild(mesInfo);
+    newMessageDiv.appendChild(mesText);
+
+    var chatPanel = window.document.getElementsByClassName('chat-screen')[0];
+
+    chatPanel.appendChild(newMessageDiv);
 }
 
 function changeUserActivity() {
@@ -154,9 +258,55 @@ function changeUserActivity() {
     userPanelHead.replaceChild(newActivity, userActivity);
 }
 
-function CreateFile()
-{
-    var fso, f1;
-    fso = new ActiveXObject("Scripting.FileSystemObject");
-    f1 = fso.CreateTextFile("c:\\testfile.txt", true);
+function ClearText(Empty) {
+    Empty.value="";
 }
+
+function click() {
+    var t = event.target;
+    var par = t.parentNode;
+    var par2 = par.parentNode;
+    var par3 = par2.parentNode;
+    return par3;
+}
+
+function getMessageText() {
+    var inputElement = document.getElementsByClassName('text-area')[0];
+    return inputElement.value;
+}
+
+function findMessageInLocalStorage(messageText) {
+    if (typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    var messageList = localStorage.getItem('messages');
+    if (messageList == undefined) {
+        return;
+    }
+    messageList = JSON.parse(messageList);
+    for(var i in messageList) {
+        if(messageList[i].text == messageText) {
+            return i;
+        }
+    }
+}
+
+function setId(length) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".split("");
+    if (! length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+    var str = "";
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+
+
+
+
+
